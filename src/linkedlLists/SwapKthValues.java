567d93b968410b2ef875a3c651b186491c7dc9e0
CalebYPI <caleb.ypi@gmail.com>
2022-05-23 22:59:30 +0200

    LinkedList updates

--- src/linkedlLists/SwapKthValues.java
+++ src/linkedlLists/SwapKthValues.java
@@ -3,7 +3,7 @@ package linkedlLists;
  * 7. Swap Kth node from beginning with Kth node from end in a Linked List
  */
 public class SwapKthValues {
-    class Node {
+    static class Node {
         int data;
         Node next;
         Node(int d)
@@ -13,20 +13,85 @@ public class SwapKthValues {
         }
     }
     
-    class LinkedList {
-        Node head;
+    static class LinkedList {
+        static Node head;
         void push(int d) {
             Node n = new Node(d);
             n.next = head;
             head = n;
         }
 
-        void showList() {
+        static void showList() {
             Node node = head;
             while (node != null) {
                 System.out.print(node.data + " ");
                 node = node.next;
             }
+            System.out.println("");
+        }
+
+        public int nodeCount() {
+            int count = 0;
+            Node node = head;
+            while (node != null) {
+                node = node.next;
+                count++;
+            }
+            return count;
+        }
+
+        public void swap(int kth) {
+            int count = nodeCount();
+            if (count < kth)
+                return;
+            if (2 * kth - 1 == count)
+                return;
+
+            Node node1 = head;
+            Node prev1 = null;
+            for (int i = 1; i < kth; i++) {
+                prev1 = node1;
+                node1 = node1.next;
+            }
+
+            Node node2 = head;
+            Node prev2 = null;
+            for (int i = 1; i < count; i++) {
+                prev2 = node2;
+                node2 = node2.next;
+            }
+            
+            if (prev1 != null)
+                prev1.next = node2;
+            
+            if (prev2 != null)
+                prev2.next = node1;
+            
+            Node temp = prev1.next;
+            prev1.next = prev2.next;
+            prev2.next = temp;
+
+            if (kth == 1)
+                head = node2;
+
+            if (kth == count)
+                head = node1;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        for (int i = 8; i >= 1; i--)
+            list.push(i);
+        
+        System.out.println("Original");
+        LinkedList.showList();
+
+        System.out.println("\n");
+        for (int i = 1; i < 9; i++) {
+            list.swap(i);
+            System.out.println("Swapped list: " + i);
+            LinkedList.showList();
             System.out.println("");
         }
     }
